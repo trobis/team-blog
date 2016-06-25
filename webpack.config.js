bc40dd359254037e9504954898838071e8df3add
leejaedus <leejaeduss@gmail.com>
2016-06-25 19:39:41 +0900

    임포트 실수를 수정함

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,7 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: [
-        'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
-        'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+        'webpack/hot/only-dev-server',
         './main.js'
     ],
     output: {path: __dirname, filename: 'bundle.js'},
